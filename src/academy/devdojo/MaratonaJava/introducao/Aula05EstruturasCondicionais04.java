e5d785647ba7c768a563a3fa48d40989afe326c0
DevID <108770371+Stefhan55@users.noreply.github.com>
2026-09-19 18:25:08 -0300

    Add files via upload

---
+++ src/academy/devdojo/MaratonaJava/introducao/Aula05EstruturasCondicionais04.java
@@ -0,0 +1,20 @@
+package academy.devdojo.MaratonaJava.introducao;
+
+public class Aula05EstruturasCondicionais04 {
+    public static void main(String[] args) {
+        double salarioAnual = 70000;
+        double primeiraFaixa = 9.70/ 100;
+        double segundaFaixa = 37.35/100;
+        double terceiraFaixa = 49.50/100;
+        double valordoImposto = 0;
+
+        if (salarioAnual <= 34712){
+            valordoImposto = salarioAnual * primeiraFaixa;
+        } else if (salarioAnual>= 34713 && salarioAnual<= 68587) {
+            valordoImposto = salarioAnual*segundaFaixa;
+        }else{
+            valordoImposto = salarioAnual*terceiraFaixa;
+        }
+        System.out.println(valordoImposto);
+    }
+}
