ddea153a28c7b0244de3e5f8d208e0d8e3ada91a
DevID <108770371+Stefhan55@users.noreply.github.com>
2026-09-21 20:14:21 -0300

    break

---
+++ src/academy/devdojo/MaratonaJava/introducao/Aula06EstruturasdeRepetiçao03.java
@@ -0,0 +1,12 @@
+public class Aula06EstruturasdeRepetiçao03 {
+    public static void main(String[] args) {
+        int valorMax = 50;
+        for (int i = 0; i <=valorMax ; i++) {
+            if (i>25){
+                break;
+            }
+            System.out.println(i);
+        }
+
+    }
+}
